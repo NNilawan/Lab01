26d90a6f20b125ce4fdc47cc771d5c2f84e84441
NNilawan <nilawan_l@cmu.ac.th>
2021-07-01 21:55:27 +0700

    Q3

--- main.js
+++ main.js
@@ -9,8 +9,16 @@ const app = Vue.createApp({
                 // 2.4
 
                 // 2.5
-                description: 'A boot is a type of footwear which covers at least the foot and usually the ankle, and sometimes extends up to the knee or even the hip. They come in every imaginable variation of height, color, material and style. Most have a heel which is clearly distinguishable from the rest of the sole, even if the two are made of one piece.'
-                    // 2.5
+                description: 'A boot is a type of footwear which covers at least the foot and usually the ankle, and sometimes extends up to the knee or even the hip. They come in every imaginable variation of height, color, material and style. Most have a heel which is clearly distinguishable from the rest of the sole, even if the two are made of one piece.',
+                // 2.5
+
+                // 3.2
+                image: './assets/images/socks_green.jpg',
+                // 3.2
+
+                // 3.6
+                link: 'https://www.camt.cmu.ac.th'
+                    // 3.6
             }
         }
     })
