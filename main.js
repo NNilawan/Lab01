d2cfb58d1c06dd5aafe189fd3fdc69e089c44e1f
Nilawan <hyacinth0666@gmail.com>
2021-07-01 21:21:16 +0700

    Q1-Q2

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const product = 'Socks'
+// const product = 'Socks'
+//2.1
+const app = Vue.createApp({
+        data() {
+            return {
+                // 2.4
+                // product: 'Socks'
+                product: 'Boots',
+                // 2.4
+
+                // 2.5
+                description: 'A boot is a type of footwear which covers at least the foot and usually the ankle, and sometimes extends up to the knee or even the hip. They come in every imaginable variation of height, color, material and style. Most have a heel which is clearly distinguishable from the rest of the sole, even if the two are made of one piece.'
+                    // 2.5
+            }
+        }
+    })
+    //2.1
